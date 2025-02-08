a79b5b597a6f4770f31a548b3e42f53ee2234c1f
Michael Ghen <mike@mikeghen.com>
2025-02-08 13:13:19 -0500

    Remove missing import

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,6 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import Proposals from "./pages/Proposals";
 import ProposalDetail from "./pages/ProposalDetail";
-import CreateAgent from "./pages/CreateAgent";
 import MyAgents from "./pages/MyAgents";
 import AgentDetail from "./pages/AgentDetail";
 import EditAgent from "./pages/EditAgent";
@@ -30,7 +29,6 @@ const App = () => (
           <Route path="/agents" element={<MyAgents />} />
           <Route path="/agents/:id" element={<AgentDetail />} />
           <Route path="/agents/:id/edit" element={<EditAgent />} />
-          <Route path="/create-agent" element={<CreateAgent />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
